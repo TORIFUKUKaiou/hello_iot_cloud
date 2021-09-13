4a1854a7448f18776bbe8b74f2c2e4c0f8850d56
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2021-09-13 18:33:51 +0900

    NOT NULL constraint

--- priv/repo/migrations/20210913093223_create_users.exs
+++ priv/repo/migrations/20210913093223_create_users.exs
@@ -3,7 +3,7 @@ defmodule HelloIotCloud.Repo.Migrations.CreateUsers do
 
   def change do
     create table(:users) do
-      add :name, :string
+      add :name, :string, null: false
 
       timestamps()
     end
